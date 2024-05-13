c91d26f336c424fc8690209d1d702057eb0d2b6a
변영화[주문&결제FE] <younghwa.byun@navercorp.com>
2024-05-13 16:58:01 +0900

    👔 getRandomValues 없는 경우 처리

--- packages/secure-random/src/secureRandom.ts
+++ packages/secure-random/src/secureRandom.ts
@@ -17,11 +17,16 @@ export const secureRandom = (option?: Option) => {
     const array = new Uint32Array(1)
     const maxNumber = Math.pow(2, 32) - 1
 
-    if (typeof window === 'undefined') {
-        return limit(global.crypto.getRandomValues(array)[0] / maxNumber)
+    try {
+        if (typeof window === 'undefined') {
+            return limit(global.crypto.getRandomValues(array)[0] / maxNumber)
+        }
+        if (typeof window?.crypto !== 'undefined') {
+            return limit(window.crypto.getRandomValues(array)[0] / maxNumber)
+        }
+    } catch (error) {
+        // do nothing
     }
-    if (typeof window?.crypto !== 'undefined') {
-        return limit(window.crypto.getRandomValues(array)[0] / maxNumber)
-    }
+
     return limit(Math.random())
 }
